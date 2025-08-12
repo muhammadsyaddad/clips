f9b50a6e0589beb69d06814d3379e238a63b85c5
smile lemon <muhamsyaddad@gmail.com>
2025-08-13 00:15:55 +0700

    chnange the typo

--- src/app/(external)/page.tsx
+++ src/app/(external)/page.tsx
@@ -1,6 +1,6 @@
 import { CardGrid } from "@/components/section/card-grid";
 import { HeroScroll } from "@/components/section/hero-scroll";
-import { ClipsNavigation } from "@/components/section/clip-navigation.tsx";
+import { ClipsNavigation } from "@/components/section/clip-navigation";
 
 const MarketingPage = () => {
   return (
